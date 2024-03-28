377c136d4966507bb138b92f4e71221b887beaef
Samuel Sanchez <wearycodes@gmail.com>
2024-03-28 01:09:43 -0500

    debugger working and catch-all endpoint created

--- api/server.js
+++ api/server.js
@@ -3,5 +3,9 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());
-
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
 module.exports = server;
