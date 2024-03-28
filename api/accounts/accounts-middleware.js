1d6d4957d9a510d377bb09a18321fc83a09df4aa
Samuel Sanchez <wearycodes@gmail.com>
2024-03-28 01:30:33 -0500

    wired routers and middlewares

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,14 +1,19 @@
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  console.log('Check Account Payload middleware')
+  // DO YOUR MAGIC 
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
-  
+  next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+console.log('Check Account Name Unique middleware')
+  // DO YOUR MAGIC 
+  next()
 }
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+console.log('Check Account ID middleware')
+  // DO YOUR MAGIC 
+  next()
 }
